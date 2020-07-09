e5b3de7cb6d004eaa60f5a28c2d8b317e18bb41b
Kiptoo Biwot <elviskb@gmail.com>
2020-07-09 12:04:10 +0300

    Populate spinner

---
+++ app/src/main/java/com/iamkipb/notekeeper/NoteActivity.java
@@ -0,0 +1,26 @@
+package com.iamkipb.notekeeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.List;
+
+public class NoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+        Spinner spinnerCourses = findViewById(R.id.spinnerCourses);
+
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses =
+                new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCourses.setAdapter(adapterCourses);
+    }
+}
