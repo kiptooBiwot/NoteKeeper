78ebc3bd548ad7e0af07580702f834fe9b8603fb
Kiptoo Biwot <elviskb@gmail.com>
2020-07-16 17:02:36 +0300

    Navigation Drawer

---
+++ app/src/main/java/com/iamkipb/notekeeper/MainActivity.java
@@ -0,0 +1,169 @@
+package com.iamkipb.notekeeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private NoteRecyclerAdapter mNoteRecyclerAdapter;
+    private RecyclerView mRecyclerItems;
+    private LinearLayoutManager mNotesLayoutManager;
+    private CourseRecyclerAdapter mCourseRecyclerAdapter;
+    private GridLayoutManager mCoursesLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NoteActivity.class));
+            }
+        });
+
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        initializeDisplayContent();
+
+//        Setting up on click listener
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                FloatingActionButton fab = findViewById(R.id.fab);
+                switch (item.getItemId()) {
+
+                    case R.id.nav_notes:
+                        displayNotes();
+                        break;
+                    case R.id.nav_course:
+                        displayCourses();
+                        break;
+                    case R.id.nav_share:
+                        fab.hide();
+                        handleSelection("Don't you think you've shared enough?");
+                        fab.show();
+                        break;
+                    case R.id.nav_send:
+                        fab.hide();
+                        handleSelection("Message Sent");
+                        fab.show();
+                        break;
+                    default:
+                        fab.show();
+                }
+                drawer.closeDrawers();
+                return false;
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mNoteRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private void initializeDisplayContent() {
+        mRecyclerItems = findViewById(R.id.list_items);
+        mNotesLayoutManager = new LinearLayoutManager(this);
+        mCoursesLayoutManager = new GridLayoutManager(this, 2);
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
+
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        mCourseRecyclerAdapter = new CourseRecyclerAdapter(this, courses);
+
+        displayNotes();
+        displayCourses();
+    }
+
+    private void displayCourses() {
+        mRecyclerItems.setLayoutManager(mCoursesLayoutManager);
+        mRecyclerItems.setAdapter(mCourseRecyclerAdapter);
+
+        selectNavigationMenuItem(R.id.nav_course);
+    }
+
+    private void displayNotes() {
+        mRecyclerItems.setLayoutManager(mNotesLayoutManager);
+        mRecyclerItems.setAdapter(mNoteRecyclerAdapter);
+
+        selectNavigationMenuItem(R.id.nav_notes);
+    }
+
+    private void selectNavigationMenuItem(int id) {
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        Menu menu = navigationView.getMenu();
+        menu.findItem(id).setChecked(true);
+    }
+
+    private void handleSelection(String message) {
+//        View view = findViewById(R.id.list_notes);
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawers();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
--- app/src/main/java/com/iamkipb/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/iamkipb/notekeeper/NoteListActivity.java
@@ -39,7 +39,6 @@ public class NoteListActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
             mNoteRecyclerAdapter.notifyDataSetChanged();
-//        mAdapterNotes.notifyDataSetChanged();
     }
 
     private void initializeDisplayContent() {
@@ -50,23 +49,35 @@ public class NoteListActivity extends AppCompatActivity {
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
         mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
         recyclerNotes.setAdapter(mNoteRecyclerAdapter);
-
-//        final ListView listNotes = findViewById(R.id.list_notes);
-
-//        List<NoteInfo> notes = DataManager.getInstance().getNotes();
-//        mAdapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
-//
-//        listNotes.setAdapter(mAdapterNotes);
-//
-//        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
-////                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
-//                intent.putExtra(NoteActivity.NOTE_POSITION, position);
-//                startActivity(intent);
-//
-//            }
-//        });
     }
 }
+
+
+//    FloatingActionButton fab = findViewById(R.id.fab);
+//    int menuId = destination.getId();
+//
+//                switch (menuId){
+//                        case R.id.nav_notes:
+//                        handleSelection("Notes selected");
+//                        fab.hide();
+//                        break;
+//                        case R.id.nav_courses:
+//                        handleSelection("Courses selected");
+//                        fab.hide();
+//                        break;
+//                        case R.id.nav_share:
+//                        handleSelection("Don't you think you've shared enough?");
+//                        fab.hide();
+//                        break;
+//                        case R.id.nav_send:
+//                        handleSelection("Message Sent");
+//                        fab.hide();
+//                        break;
+//default:
+//        fab.show();
+//        }
+
+//    private void handleSelection(String message) {
+//        View view = findViewById(R.id.list_items);
+//        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
+//    }
